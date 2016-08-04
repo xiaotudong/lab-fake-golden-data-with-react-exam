1fd01e7f47c2bacb79676d5b07196aff7bd86439
dingqian <302965061@qq.com>
2016-08-04 22:51:07 +0800

    add addElement

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+
 const App = React.createClass({
     getInitialState:function () {
         return{
-            isEditor:true
+            isEditor:true,
+            elements:[]
         }
     },
     toggle:function () {
@@ -9,26 +11,59 @@ const App = React.createClass({
             isEditor:!this.state.isEditor
         })
     },
+    addElement:function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
     render: function () {
         const isEditor = this.state.isEditor;
         return <div>
-            <button  onClick={this.toggle}>{isEditor ? "preview":"edit"}</button>
-            <div className={isEditor? "":"hidden"}>
-                <Editor />
+            <button onClick={this.toggle}>{isEditor ? "preview" : "edit"}</button>
+            <div className={isEditor ? "" : "hidden"}>
+                <Editor elements={this.state.elements} onAdd = {this.addElement}/>
             </div>
-            <div className={isEditor? "hidden":""}>
+            <div className={isEditor ? "hidden" : ""}>
                 <Previewer />
             </div>
         </div>
     }
 });
 const Editor = React.createClass({
-    render:function() {
+    render: function () {
         return <div>
-            editor
+            <Left elements={this.props.elements}/>
+            <Right onAdd = {this.props.onAdd} />
         </div>
     }
 });
+
+const Right = React.createClass({
+    add:function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
+    render:function() {
+        return <div>
+            <input type = "radio" name="element" value = "text" />text
+            <input type = "radio" name="element" value = "date" />date
+            <button onClick={this.add}>+</button>
+        </div>
+    }
+});
+ const Left = React.createClass({
+     render:function() {
+         const elements = this.props.elements.map((ele,index) => {
+             return <div key ={index}>
+                 <input type={ele}/>
+                 <button>x</button>
+             </div>;
+         });
+         return <div>
+             {elements}
+         </div>
+     }
+ });
 const Previewer = React.createClass({
     render:function() {
         return <div>
@@ -37,3 +72,4 @@ const Previewer = React.createClass({
     }
 });
 ReactDOM.render(<App />, document.getElementById('content'));
+ 
