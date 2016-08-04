4093b3e3f3ed06f7743d9430f56d38d37860954a
dingqian <302965061@qq.com>
2016-08-04 22:59:33 +0800

    add preview

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
-
 const App = React.createClass({
-    getInitialState:function () {
-        return{
-            isEditor:true,
-            elements:[]
+    getInitialState: function () {
+        return {
+            isEditor: true,
+            elements: []
         }
     },
-    toggle:function () {
+    toggle: function () {
         this.setState({
-            isEditor:!this.state.isEditor
+            isEditor: !this.state.isEditor
         })
     },
-    addElement:function (element) {
+    addElement: function (element) {
         const elements = this.state.elements;
         elements.push(element);
         this.setState({elements});
     },
-    deleteElement:function (index) {
+    deleteElement: function (index) {
         const elements = this.state.elements;
-        elements.splice(index,1);
+        elements.splice(index, 1);
         this.setState({elements});
     },
     render: function () {
@@ -26,10 +25,10 @@ const App = React.createClass({
         return <div>
             <button onClick={this.toggle}>{isEditor ? "preview" : "edit"}</button>
             <div className={isEditor ? "" : "hidden"}>
-                <Editor elements={this.state.elements} onAdd = {this.addElement} onDelete = {this.deleteElement}/>
+                <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.deleteElement}/>
             </div>
             <div className={isEditor ? "hidden" : ""}>
-                <Previewer />
+                <Previewer elements={this.state.elements}/>
             </div>
         </div>
     }
@@ -37,45 +36,53 @@ const App = React.createClass({
 const Editor = React.createClass({
     render: function () {
         return <div>
-            <Left elements={this.props.elements} onDelete = {this.props.onDelete}/>
-            <Right onAdd = {this.props.onAdd} />
+            <Left elements={this.props.elements} onDelete={this.props.onDelete}/>
+            <Right onAdd={this.props.onAdd}/>
         </div>
     }
 });
 
 const Right = React.createClass({
-    add:function () {
+    add: function () {
         const element = $("input[name=element]:checked").val();
         this.props.onAdd(element);
     },
-    render:function() {
+    render: function () {
         return <div>
-            <input type = "radio" name="element" value = "text" />text
-            <input type = "radio" name="element" value = "date" />date
+            <input type="radio" name="element" value="text"/>text
+            <input type="radio" name="element" value="date"/>date
             <button onClick={this.add}>+</button>
         </div>
     }
 });
- const Left = React.createClass({
-     remove:function (index) {
-         this.props.onDelete(index);
-     },
-     render:function() {
-         const elements = this.props.elements.map((ele,index) => {
-             return <div key ={index}>
-                 <input type={ele}/>
-                 <button onClick={this.remove.bind(this,index)}>x</button>
-             </div>;
-         });
-         return <div>
-             {elements}
-         </div>
-     }
- });
-const Previewer = React.createClass({
-    render:function() {
+const Left = React.createClass({
+    remove: function (index) {
+        this.props.onDelete(index);
+    },
+    render: function () {
+        const elements = this.props.elements.map((ele, index) => {
+            return <div key={index}>
+                <input type={ele}/>
+                <button onClick={this.remove.bind(this, index)}>x</button>
+            </div>;
+        });
         return <div>
-            previewer
+            {elements}
+        </div>
+    }
+});
+const Previewer = React.createClass({
+    render: function () {
+        const elements = this.props.elements.map((ele, index) => {
+            return <div key={index}>
+
+                <input type={ele}/>
+            </div>;
+
+        });
+        return <div>
+            {elements}
+            <button>submit</button>
         </div>
     }
 });
