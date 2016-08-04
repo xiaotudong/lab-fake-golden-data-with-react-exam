920813c6e7f6c36fcdc5cbd8587878212611b395
dingqian <302965061@qq.com>
2016-08-04 21:16:24 +0800

    add previewer and editor

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-// TODO
+const App = React.createClass({
+    getInitialState:function () {
+        return{
+            isEditor:true
+        }
+    },
+    toggle:function () {
+        this.setState({
+            isEditor:!this.state.isEditor
+        })
+    },
+    render: function () {
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button  onClick={this.toggle}>{isEditor ? "preview":"edit"}</button>
+            <div className={isEditor? "":"hidden"}>
+                <Editor />
+            </div>
+            <div className={isEditor? "hidden":""}>
+                <Previewer />
+            </div>
+        </div>
+    }
+});
+const Editor = React.createClass({
+    render:function() {
+        return <div>
+            editor
+        </div>
+    }
+});
+const Previewer = React.createClass({
+    render:function() {
+        return <div>
+            previewer
+        </div>
+    }
+});
+ReactDOM.render(<App />, document.getElementById('content'));
